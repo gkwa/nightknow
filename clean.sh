7613266719c4a01018725d56e360a90ec801b066
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-23 16:35:50 -0800

    Don't leave CRDs around after uninsatlling helm chart

--- clean.sh
+++ clean.sh
@@ -7,4 +7,9 @@ kubectl delete --wait -f subnet.yaml -f vpc.yaml
 kubectl delete --wait -f role.yaml -f policy.yaml -f rolepolicyattachment.yaml
 helm uninstall --wait crossplane -n crossplane-system
 kubectl delete --wait namespace crossplane-system
+
+# Helm does not delete CRD objects. You can delete the ones Crossplane
+# created with the following commands:
+kubectl patch lock lock -p '{"metadata":{"finalizers": []}}' --type=merge kubectl get crd -o name | grep crossplane.io | xargs kubectl delete
+
 kind delete cluster --name nightknow
